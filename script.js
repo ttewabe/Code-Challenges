bac7f775864ff2c972fa714fe38882d3c1047925
Tewabe <32206949+winneramsalu@users.noreply.github.com>
2022-03-27 23:58:46 -0700

    Arrow Function Writing Drills

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const inputEl = document.querySelector("input");
-// Add your event listener here
-inputEl.addEventListener("onkeyup",fixLength)
+// Drill #1: Change the below function declaration to an arrow function
+ let ftToInches = ft => ft * 12;
+ 
+// Test - you do not need to change this part.
+console.log(`5 feet equals: ${ftToInches(5)} inches.`);
 
-function fixLength() {
-  
-  if (inputEl.value.length > 12) {
-   
-    inputEl.value = inputEl.value.slice(0,12);
-  
-  } 
-}
+// Drill #2: Change the below function declaration to an arrow function
+let area = (width, length) => width * length;
+// Test - you do not need to change this part.
+console.log(`The area of a 4x5 rectangle is: ${area(4, 5)}.`);
+
+// Drill #3: Change the below function expression to an arrow function
+const daysToHours = days => days * 24;
+
+// Test - you do not need to change this part.
+console.log(`28 days is equal to ${daysToHours(28)} hours.`);
+
+// Drill #4: Change the below function expression to an arrow function
+const cardName = (value, suit) => `${value} of ${suit}`;
+// Test - you do not need to change this part.
+console.log(`You turn the card over to reveal a ${cardName(7, "Clubs")}!`);
