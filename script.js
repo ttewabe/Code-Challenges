b847d8f01dcaa729a824f6d3b9611078a1017cb8
Tewabe <32206949+winneramsalu@users.noreply.github.com>
2022-03-28 00:21:50 -0700

    Array Filter Method

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
-const pets = ["goldfish", "dog", "turtle", "tiger"]
+const restaurants = [
+  {
+    "name": "La Patisserie",
+    "takeout": false,
+    "rating": 5
+  },
+  {
+    "name": "Cafe Geneva",
+    "takeout": true,
+    "rating": 6
+  },
+  {
+    "name": "NuCafe",
+    "takeout": true,
+    "rating": 8
+  },
+  {
+    "name": "Bob's Seafood",
+    "takeout": false,
+    "rating": 2
+  }  
+];
 
-//1 
-const petsShortNames = pets.filter(pets => pets.length < 6)
-// The below line should console.log: ["dog", "tiger"]
-console.log(petsShortNames);
+document.getElementById("filterTakeoutBtn").addEventListener("click", filterTakeout);
+document.getElementById("filterRatings").addEventListener("click", filterRatings);
+document.getElementById("filterName").addEventListener("click", filterName);
 
-//2 - uncomment line 9 and line 11 when you've finished challenge 1
-const pets1 = ["goldfish", "dog", "turtle", "tiger"]
-const petsTNames = pets1.filter( pets1 => pets1.includes("t"))
-// The below line should console.log: ["turtle", "tiger"] 
-console.log(petsTNames);
+function filterTakeout() {
+  let rList = "\nThese restaurants offer takeout:\n";
+  // The below line creates a new array with only the restaurants that offer takeout
+  const hasTakeout = restaurants.filter(restaurant => restaurant.takeout);
+  
+  // The below line takes the restaurant names from the array and makes a string
+  hasTakeout.forEach(restaurant => { 
+    rList += `\n${restaurant.name}`;
+  });
+  
+  // The below line updates the DOM with the newly created string
+  document.getElementById("list").innerText = rList;
+}
 
-
-//3 - uncomment line 15 and line 17
-const numbers = [3, 7, 4, 5, 10]; 
-const numbersBiggerThanFour = numbers.filter(numbers => numbers > 4)
-// The below line should console.log: [7, 5, 10]
-console.log(numbersBiggerThanFour);
-//Bonus - uncomment line 20 and line 22
-const number = [3, 7, 4, 5, 10]; 
-const evenNumbers = number.filter( number => number % 2 === 0)
-//The below line should console.log: [4, 10]
-console.log(evenNumbers);
-
+// Filter By Rating
+function filterRatings() {
+  let rList = "\nThese restaurants get rating:\n";
+  const hasRating = restaurants.filter(restaurant => restaurant.rating >= 5);
+  hasRating.forEach(restaurant => { 
+    rList += `\n${restaurant.name}`;
+  });
+  
+  document.getElementById("list").innerText = rList;
+}
+// Filter By Name
+function filterName() {
+  let rList = "\nThese restaurants get name:\n";
+  const hasName = restaurants.filter(restaurant => restaurant.name.startsWith("C") );
+  hasName.forEach(restaurant => { 
+    rList += `\n${restaurant.name}`;
+  });
+  
+  document.getElementById("list").innerText = rList;
+}
