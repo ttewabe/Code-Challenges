470bce291df0c013edca3be6cab043170f25e15b
Tewabe <32206949+winneramsalu@users.noreply.github.com>
2022-03-27 23:55:13 -0700

    MicroTweet

--- script.js
+++ script.js
@@ -1,53 +1,12 @@
-class Monster {
-    constructor(type, color) {
-        this.type = type;
-        this.color = color;
-        this.isScary = true;
-    }
-    roar() {
-        console.log(`The ${this.color} ${this.type} lets out a tremendous roar!`);
-    }
+const inputEl = document.querySelector("input");
+// Add your event listener here
+inputEl.addEventListener("onkeyup",fixLength)
+
+function fixLength() {
+  
+  if (inputEl.value.length > 12) {
+   
+    inputEl.value = inputEl.value.slice(0,12);
+  
+  } 
 }
-
-class Dragon extends Monster {
-    constructor(type, color, element) {
-        super(type, color);
-        this.element = element;
-    }
-    fly() {
-        console.log(`The ${this.color} ${this.element} ${this.type} flaps its wings and begins to fly.`);
-    }
-   poison() {
-            console.log(`The ${this.color} ${this.element} ${this.type} blood and fangs are very poison to human.`);
- }
-}
-
-class Werewolf extends Monster {
-    constructor(type, color) {
-        super(type, color);
-    }
-    howl() {
-        console.log(`The ${this.color} ${this.type} howls loudly.`);
-    }
-}
-
-class Lizard extends Monster {
-    constructor(type, color, size) {
-       super(type, color);
-      this.size = size;
-    }
-    howl() {
-        console.log(`The ${this.size}  ${this.color} ${this.type} bites is not fatal to humans.`);
-    }
-}
-
-const woodDragon = new Dragon("dragon", "brown", "wood");
-woodDragon.roar()
-woodDragon.fly()
-woodDragon.poison()
-
-const werewolf1 = new Werewolf("werewolf", "golden");
-werewolf1.howl();
-
-const lizard= new Lizard("lizard", "grey", "large");
-lizard.howl();
