fc29938ca85f45e239cc5624ad56d661024c9e37
Tewabe <32206949+winneramsalu@users.noreply.github.com>
2022-03-28 00:03:20 -0700

    Objects

--- script.js
+++ script.js
@@ -1,77 +1,49 @@
-/****************************** Challenge 1 ******************************/
-const getInitials = (firstName, lastName) => firstName[0] + lastName[0]; 
-console.log(getInitials("Charlie", "Brown")); // this should console.log "CB"
-
-
-/****************************** Challenge 2 ******************************/
-function getStudentGrade(finalExamPoints, assignmentsPoints) {
-  const percent = ((finalExamPoints + assignmentsPoints) / 300) * 100; 
-  let grade = "Unknown";
-  if (percent >= 90) {
-    grade = "A";
+// 1 - white rabbit
+const rabbit = { 
+  color: "white",
+  checkWatch() {
+    console.log(`A ${this.color} rabbit checks his watch and exclaims, 'I\'m late!'`);
   }
-  if (percent >= 80 && percent < 90) {
-    grade =  "B";
-  } 
-  if (percent >= 70 && percent < 80) {
-    grade =  "C";
-  } 
-  if (percent >= 60 && percent < 70) {
-    grade =  "D";
-  } 
-  if (percent < 60) {
-    grade =  "F";
-  }
-  return grade;
-}
-console.log(getStudentGrade(70, 180)); // this should console.log "B"
-
-/****************************** Challenge 3 ******************************/
-function digitToWord (digit){ 
-  switch (digit) {
-    case 0: return "zero"; break;
-    case 1: return "one"; break;
-    case 2: return "two"; break;
-    case 3: return "three"; break;
-    case 4: return "four"; break;
-    case 5: return "five"; break;
-    case 6: return "six"; break;
-    case 7: return "seven"; break;
-    case 8: return "eight"; break;
-    case 9: return "nine"; break;
-    default: return "unknown";
-  };
-}
-const word = digitToWord(5);
-console.log("5 is " + word); // This should console.log "5 is five"
-
-
-/****************************** Challenge 4 ******************************/
-function hasTreat(treat) {
-  const treatsArr = ['cookie', 'cake', 'muffin', 'pie', 'ice cream'];
-  if (treatsArr.indexOf(treat) ===0) {
-    return true;
-  }
-  return false;
-}
-if (hasTreat("cookie")) { // You should have a cookie in the treatsArr array.
-  console.log("You have a cookie!");
-} else {
-  console.log("You have no cookie."); // This is wrong. You should have a cookie. 
-}
-/* Tip: The problem you are trying to resolve here is why the function returns false
-   for when you are checking to see if "cookie" exists in the treatsArr array.
-   Do not alter the treatsArr array to resolve this, and do not alter the hasTreat()
-   function to ONLY return true for an argument of "cookie" - it should return true
-   for an argument of any item in the treatsArr array. The code you need to fix is
-   is in the "if" condition. */
-
-
-/****************************** Challenge 5 ******************************/
-const randomValue = (min, max) => { 
-  const val = Math.random() * (max - min) + min;
-  return val;
 };
-console.log(randomValue(2,6)) // should return a random numeric value between 2 and 6. 
-// For an explanation of Math.random, see: 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_number_between_two_values
+rabbit.checkWatch();
+
+// 2 - spaceship
+const spaceship = { 
+  name: "Apolo",
+  launch() {
+    alert(`You have launched the spaceship ${this.name}!`)
+  }
+};
+  spaceship.launch(); 
+
+// 3 - shopping cart
+const cart = {
+  contents:[],
+  addItem(item) {
+    cart.contents.push(item);
+  }
+};
+
+cart.addItem("laptop");
+console.log("The cart contains:", cart.contents);
+
+// 4 - lever
+// your code here - uncomment the line below when done
+const lever ={
+  stamp: "ACME",
+  pull() {
+    console.log(`An anvil stamped ${this.stamp} drops on your head.`)
+  }
+}
+lever.pull();
+
+// 5 - Table
+const dog ={
+  name:"Popy",
+  age: "four",
+  color: "Black",
+  woow(){
+    console.log(`My dog ${this.name} is a ${this.color} color and he is ${this.age} years old.`)
+  }
+}
+dog.woow()
